7d51c269b29abc09e4d02aa31443a9be8e801b61
vmaguiar <vmaguiar@hotmail.com>
2023-08-18 17:02:05 -0300

    feat: creates acessible modal w/ radix ui dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
 import logoImg from '../../assets/logo.svg'
+
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
@@ -10,7 +13,21 @@ export function Header() {
         <HeaderContent>
           <img src={logoImg} alt="" />
 
-          <NewTransactionButton>Nova Transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay />
+
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </div>
